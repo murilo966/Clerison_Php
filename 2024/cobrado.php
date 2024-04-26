760a5eac5677b0219da8785256eab623fcb42400
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-04-26 12:35:29 -0300

    colocando uma página para tudo q o clerison pedir

---
+++ 2024/cobrado.php
@@ -0,0 +1,14 @@
+<?php
+$user = 'joaquim';
+$senha = 12345;
+echo "usuário: $user e senha: $senha <br><br>";
+
+if ($user == 'joaquim' && $senha == 12345) {
+    echo 'login efetuado com sucesso';
+} else {
+    echo 'você é um incapacitado. Não consegue entrar no site pq é burro e pôs os dados errados';
+}
+
+
+
+?>
