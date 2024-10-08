ae66e9a4a189ef4c742b46f592333dd3fd3ff247
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-10-08 12:33:06 -0300

    criando os botões na home

--- 2024/Crud/inicio.php
+++ 2024/Crud/inicio.php
@@ -18,6 +18,8 @@ if (empty($_SESSION['email'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     
@@ -27,10 +29,48 @@ if (empty($_SESSION['email'])) {
 
     <?php 
         $dados = BuscarUsuário();
-        echo"<pre>";
-            print_r($dados);
-        echo"</pre>"
     
     ?>
+    <div class="container text-center">
+        <div class="row border border-2">
+            <div class="col-md-3">
+                <h3>ID</h3>
+            </div>
+            <div class="col-md-3">
+                <h3>NOME</h3>
+            </div>
+            <div class="col-md-3">
+                <h3>E-MAIL</h3>
+            </div>
+            <div class="col-md-3">
+                <h3>BOTÕES</h3>
+            </div>
+        </div>
+        
+        
+        <?php
+            foreach ($dados as $key => $value) {
+                echo"<div class='row border border-2'>";
+                    echo"<div class='col-md-3'>";
+                        echo $dados[$key]['id'];
+                    echo"</div>";
+                    echo"<div class='col-md-3'>";
+                        echo $dados[$key]['nome'];
+                    echo "</div>";
+                    echo"<div class='col-md-3'>";
+                        echo $dados[$key]['email'];
+                    echo"</div>";
+                    echo"<div class='col-md-3'>";
+                        echo "<a class='btn btn-primary'href='editar.php'>Editar</a>";
+                        echo "<a class='btn btn-danger'href='excluir.php?id=".$dados[$key]['id']."'>Excluir</a>";
+                    echo "</div>";
+                echo"</div>";
+                
+            }
+
+        ?>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
 </body>
 </html>
